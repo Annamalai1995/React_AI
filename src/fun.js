e45f48a91273713b243cb1384ec47eefea0c6482
Annamalai1995 <s.annamalai1995@gmail.com>
2025-06-16 10:53:32 +0530

    Function

---
+++ src/fun.js
@@ -0,0 +1,41 @@
+export function List()
+{
+    return(
+        
+        <ul>
+            <li>Deva</li>
+            <li>Lenin</li>
+            <li>Balaji</li>
+
+        </ul>
+
+        
+    );
+
+
+
+}
+function Flow()
+{
+    return(
+        <>
+        <p>Struggling will Always Have a Positive End</p>
+        </>
+
+    )
+
+}  
+export let Orderlist=()=>
+{
+    return(
+        <>
+        <ol>
+            <li>Priya</li>
+           <li> Gowthami</li>
+           <li>Vidhya</li>
+           </ol>
+        </>
+    );
+}
+
+export{Flow};
